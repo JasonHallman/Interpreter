94e6a3d9035b7267addb24d27c600865f62b007b
Jason Hallman <jason.hallman@aol.com>
2024-06-25 10:43:29 -0400

    Add additional Lexer tests

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -29,3 +29,67 @@ TEST_CASE("Lexer test", "[lexer]") {
         }
     }
 }
+
+TEST_CASE("More", "[lexer]") {
+    SECTION("Default") {
+        std::string input =
+            "let five = 5; \
+            let ten = 10; \
+            \
+            let add = fn(x, y) { \
+            x + y; \
+            }; \
+            \
+            let result = add(five, ten); \
+        "
+
+        Token test_tokens[] = {
+            Token(TokenType::LET, "let"),
+            Token(TokenType::IDENT, "five"),
+            Token(TokenType::ASSIGN, "="),
+            Token(TokenType::INT, "5"),
+            Token(TokenType::SEMICOLON, ";"),
+            Token(TokenType::LET, "let"),
+            Token(TokenType::IDENT, "ten"),
+            Token(TokenType::ASSIGN, "="),
+            Token(TokenType::INT, "10"),
+            Token(TokenType::SEMICOLON, ";"),
+            Token(TokenType::LET, "let"),
+            Token(TokenType::IDENT, "add"),
+            Token(TokenType::ASSIGN, "="),
+            Token(TokenType::FUNCTION, "fn"),
+            Token(TokenType::LPAREN, "("),
+            Token(TokenType::IDENT, "x"),
+            Token(TokenType::COMMA, ","),
+            Token(TokenType::IDENT, "y"),
+            Token(TokenType::RPAREN, ")"),
+            Token(TokenType::LBRACE, "{"),
+            Token(TokenType::IDENT, "x"),
+            Token(TokenType::PLUS, "+"),
+            Token(TokenType::IDENT, "y"),
+            Token(TokenType::SEMICOLON, ";"),
+            Token(TokenType::RBRACE, "}"),
+            Token(TokenType::SEMICOLON, ";"),
+            Token(TokenType::LET, "let"),
+            Token(TokenType::IDENT, "result"),
+            Token(TokenType::ASSIGN, "="),
+            Token(TokenType::IDENT, "add"),
+            Token(TokenType::LPAREN, "("),
+            Token(TokenType::IDENT, "five"),
+            Token(TokenType::COMMA, ","),
+            Token(TokenType::IDENT, "ten"),
+            Token(TokenType::RPAREN, ")"),
+            Token(TokenType::SEMICOLON, ";"),
+            Token(TokenType::EOF_, ""),
+        };
+
+        Lexer *l = new Lexer(input);
+
+        for (int i = 0; i < input.size(); i++) {
+            Token t = l->nextToken();
+
+            REQUIRE(t.type == test_tokens[i].type);
+            REQUIRE(t.literal == test_tokens[i].literal);
+        }
+    }
+}
